3d19f8b3a079984e7c74a43085a3ca25b43a197e
ajcaicedo <ajcaicedo007@gmail.com>
2023-08-01 12:01:22 -0500

    este es el commit del modulo 9

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,13 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var resultado = [];
+   for(i in objeto){
+      var valor = objeto[i];
+      var arreglo = [i, valor];
+      resultado.push(arreglo);
+   }
+   return resultado;
 }
 
 function numberOfCharacters(string) {
@@ -14,7 +21,28 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
-}
+   const charCount = {};
+
+   // Recorremos el string.
+   for (let i = 0; i < string.length; i++) {
+     const char = string[i].toLowerCase(); 
+     
+
+     if (charCount[char]) {
+       charCount[char]++;
+     } else {
+
+       charCount[char] = 1;
+     }
+   }
+
+   const sortedChars = {};
+   Object.keys(charCount).sort().forEach((key) => {
+     sortedChars[key] = charCount[key];
+   });
+ 
+   return sortedChars;
+ }
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
@@ -22,6 +50,20 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var mayusc = "";
+   var minusc = "";
+ 
+   for (var i = 0; i < string.length; i++) {
+     var caract = string[i];
+     if (caract.toUpperCase() === caract) {
+      mayusc += caract;
+     } else {
+      minusc += caract;
+     }
+   }
+ 
+   return mayusc + minusc;
+ 
 }
 
 function asAmirror(frase) {
@@ -29,19 +71,44 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
-}
+   var frasenuev = "";
+   var arreglo = frase.split(" ");
+   for(i = 0; i< arreglo.length; i++){
+      var palabra = arreglo[i].split("");
+      var palabrareversa = palabra.reverse();
+      nuevapalabra = palabrareversa.join("")
+      if (i === 0) {
+         frasenuev = nuevapalabra;
+       } else {
+         frasenuev = frasenuev + " " + nuevapalabra; 
+       }
+     }
+   
+     return frasenuev;
+   }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
-}
+   const numeroStr = numero.toString();
+   const numeroReverso = numeroStr.split("").reverse().join("");
+ 
+   if (numeroStr === numeroReverso) {
+     return "Es capicua";
+   } else {
+     return "No es capicua";
+   }
+ }
+ 
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   return string.replace(/[abc]/g, "");
 }
+
 
 function sortArray(arrayOfStrings) {
    // Recibes un arreglo de strings.
@@ -49,6 +116,13 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   const arregloorden = arrayOfStrings.slice();
+
+   arregloorden.sort((a, b) => a.length - b.length);
+ 
+   console.log(arregloorden);
+   return arregloorden;
+
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +132,22 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   const intersection = [];
+
+   for (let i = 0; i < array1.length; i++) {
+     const currentElement = array1[i];
+ 
+     // Check if the current element is present in array2
+     if (array2.includes(currentElement)) {
+       // Check if the element is already in the intersection array to avoid duplicates
+       if (!intersection.includes(currentElement)) {
+         intersection.push(currentElement);
+       }
+     }
+   }
+ 
+   return intersection;
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
